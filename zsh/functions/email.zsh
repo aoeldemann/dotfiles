3d72d768546bd4aa28597ea97d002165f2f2a618
Andreas Oeldemann <hey@aoel.io>
2019-02-09 23:51:26 +0100

    zsh: updated functions

--- zsh/functions/email.zsh
+++ zsh/functions/email.zsh
@@ -3,6 +3,22 @@ function email {
     echo "ERROR: no domain set"
     return
   fi
-  prefix=`LC_ALL=C tr -dc 'a-f0-9' < /dev/urandom | head -c 16`
-  echo "${prefix}@${EMAIL_DOMAIN}"
+
+  if [[ -z $1 ]]; then
+    echo -n "ID (optional): "
+    read id
+  else
+    id=$1
+  fi
+
+  prefix=`LC_ALL=C tr -dc 'a-f0-9' < /dev/urandom | head -c 8`
+
+  if [[ -z $id ]]; then
+    addr=${prefix}@${EMAIL_DOMAIN}
+  else
+    addr=${prefix}-${id}@${EMAIL_DOMAIN}
+  fi
+
+  echo $addr
+  echo `date +"%y/%m/%d %H:%M"` $addr >> ~/.email_history
 }
